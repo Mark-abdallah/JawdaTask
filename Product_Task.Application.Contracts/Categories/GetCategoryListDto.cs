660d0968223865b01cf70cfff289b935fdf85c9e
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-11-18 11:18:23 +0000

    Delete Product_Task.Application.Contracts directory

--- Product_Task.Application.Contracts/Categories/GetCategoryListDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using Volo.Abp.Application.Dtos;
-
-namespace Product_Task.Categories
-{
-    public  class GetCategoryListDto : PagedAndSortedResultRequestDto
-    {
-        public string Filter { get; set; }
-    }
-}
